1bf1d196c1602a247fa522d8fe2fb5d886554b41
Rob Long <bobalong@gmx.net>
2021-06-16 12:42:49 +0100

    Enable modules

--- main.tf
+++ main.tf
@@ -36,32 +36,32 @@ provider "azuread" {
 }
 
 
-// module "windows-server" {
-//     source  =   "./azure/WindowsServer2019"
-// }
+module "windows-server" {
+    source  =   "./azure/WindowsServer2019"
+}
 
-// module "winvm" {
-//     source =    "./azure/Windows10-ExistingInfra"
-// }
-
-
-
-// module "aks" {
-//     source    =     "./azure/aks"
-//     env       =     "dev"
-// }
+module "winvm" {
+    source =    "./azure/Windows10-ExistingInfra"
+}
 
 
 
-// module "customrole" {
-//     source  =   "./azure/custom-roles"
-// }
+module "aks" {
+    source    =     "./azure/aks"
+    env       =     "dev"
+}
 
-// module "provisioners" {
-//     source  =   "./azure/provisioners-example"
-// }
 
-// module "linuxvm" {
-//     source  =   "./azure/linuxVM"
-// }
+
+module "customrole" {
+    source  =   "./azure/custom-roles"
+}
+
+module "provisioners" {
+    source  =   "./azure/provisioners-example"
+}
+
+module "linuxvm" {
+    source  =   "./azure/linuxVM"
+}
 
